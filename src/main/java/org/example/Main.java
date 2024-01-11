816198c92f74fafb4a544616ad5fbd7f6bef4f3d
Reddyong <d0209hong@naver.com>
2024-01-11 16:53:01 +0900

    feat : practice Singleton pattern

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.design.singleton.AClazz;
+import org.example.design.singleton.BClazz;
+import org.example.design.singleton.SocketClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        AClazz aClazz = new AClazz();
+        BClazz bClazz = new BClazz();
+
+        SocketClient aClient = aClazz.getSocketClient();
+        SocketClient bClient = bClazz.getSocketClient();
+
+        System.out.println("두 개의 객체가 동일한가?");
+        System.out.println(aClient.equals(bClient));
+
     }
 }
