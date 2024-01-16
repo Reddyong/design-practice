dfe01acf3151231a07c6e30d141bc9d600292dd3
Reddyong <d0209hong@naver.com>
2024-01-16 18:26:56 +0900

    refactor : main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,105 +22,112 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Main {
     public static void main(String[] args) {
 
-//        AClazz aClazz = new AClazz();
-//        BClazz bClazz = new BClazz();
-//
-//        SocketClient aClient = aClazz.getSocketClient();
-//        SocketClient bClient = bClazz.getSocketClient();
-//
-//        System.out.println("두 개의 객체가 동일한가?");
-//        System.out.println(aClient.equals(bClient));
+        // singleton pattern
+        AClazz aClazz = new AClazz();
+        BClazz bClazz = new BClazz();
 
-//        HairDryer hairDryer = new HairDryer();
-//        connect(hairDryer);
-//
-//        Cleaner cleaner = new Cleaner();
-//        Electronic110V adapter = new SocketAdapter(cleaner);
-//        connect(adapter);
-//
-//        AirConditioner airConditioner = new AirConditioner();
-//        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-//        connect(airAdapter);
+        SocketClient aClient = aClazz.getSocketClient();
+        SocketClient bClient = bClazz.getSocketClient();
 
-//        Browser browser = new Browser("www.naver.com");
-//        browser.show();
+        System.out.println("두 개의 객체가 동일한가?");
+        System.out.println(aClient.equals(bClient));
 
-//        IBrowser browser = new BrowserProxy("www.naver.com");
-//        browser.show();
-//        browser.show();
-//        browser.show();
-//        browser.show();
-//        browser.show();
+        // Adapter pattern
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
 
-//        AtomicLong start = new AtomicLong();
-//        AtomicLong end = new AtomicLong();
-//
-//        IBrowser aop = new AopBrowser("www.naver.com",
-//                () -> {
-//                    System.out.println("before");
-//                    start.set(System.currentTimeMillis());
-//                },
-//                () -> {
-//                    long now = System.currentTimeMillis();
-//                    end.set(now - start.get());
-//                });
-//
-//        aop.show();
-//        System.out.println("loading time : " + end.get());
-//
-//        aop.show();
-//        System.out.println("loading time : " + end.get());
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
 
-//        ICar audi = new Audi(1000);
-//        audi.showPrice();
-//
-//        // a3
-//        ICar audi3 = new A3(audi, "A3");
-//        audi3.showPrice();
-//
-//        // a4
-//        ICar audi4 = new A4(audi, "A4");
-//        audi4.showPrice();
-//
-//        // a5
-//        ICar audi5 = new A5(audi, "A5");
-//        audi5.showPrice();
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
 
-//        Button button = new Button("버튼");
-//
-//        button.addListener(new IButtonListener() {
-//            @Override
-//            public void clickEvent(String event) {
-//                System.out.println(event);
-//            }
-//        });
-//        button.click("메시지 전달 : click1");
-//        button.click("메시지 전달 : click2");
-//        button.click("메시지 전달 : click3");
-//        button.click("메시지 전달 : click4");
+        // Proxy Pattern
+        Browser browser1 = new Browser("www.naver.com");
+        browser1.show();
 
-//        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
-//        ftpClient.connect();
-//        ftpClient.moveDirectory();
-//
-//        Writer writer = new Writer("text.tmp");
-//        writer.fileConnect();
-//        writer.write();
-//
-//        Reader reader = new Reader("text.tmp");
-//        reader.fileConnect();
-//        reader.fileRead();
-//
-//        reader.fileDisconnect();
-//        writer.fileDisconnect();
-//        ftpClient.disconnect();
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
 
-//        SftpClient sftpClient = new SftpClient("foo.co.kr", 22, "/home/etc", "text.tmp");
-//        sftpClient.connect();
-//        sftpClient.write();
-//        sftpClient.read();
-//        sftpClient.disconnect();
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
+        IBrowser aop = new AopBrowser("www.naver.com",
+                () -> {
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                () -> {
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                });
+
+        aop.show();
+        System.out.println("loading time : " + end.get());
+
+        aop.show();
+        System.out.println("loading time : " + end.get());
+
+        // Decorator pattern
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        // a3
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+
+        // a4
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+
+        // a5
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice();
+
+        // Observer pattern
+        Button button = new Button("버튼");
+
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+        button.click("메시지 전달 : click1");
+        button.click("메시지 전달 : click2");
+        button.click("메시지 전달 : click3");
+        button.click("메시지 전달 : click4");
+
+        // Facade pattern
+        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
+
+        Writer writer = new Writer("text.tmp");
+        writer.fileConnect();
+        writer.write();
+
+        Reader reader = new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        reader.fileDisconnect();
+        writer.fileDisconnect();
+        ftpClient.disconnect();
+
+        SftpClient sftpClient = new SftpClient("foo.co.kr", 22, "/home/etc", "text.tmp");
+        sftpClient.connect();
+        sftpClient.write();
+        sftpClient.read();
+        sftpClient.disconnect();
+
+        // Strategy pattern
         Encoder encoder = new Encoder();
 
         // base64
